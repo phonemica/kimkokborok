3b80f9ff23f770be05debdae6f0a836058cf22a1
KP van Dam 柯禕藍 <patkaiist@gmail.com>
2025-06-24 08:03:24 +0200

    formatting

--- lexibank_kimkokborok.py
+++ lexibank_kimkokborok.py
@@ -5,6 +5,7 @@ from pylexibank import Dataset as BaseDataset
 from pylexibank import progressbar as pb
 from pylexibank import FormSpec
 
+
 class Dataset(BaseDataset):
     dir = pathlib.Path(__file__).parent
     id = "kimkokborok"
@@ -20,54 +21,24 @@ class Dataset(BaseDataset):
         for concept in self.conceptlists[0].concepts.values():
             idx = concept.id.split("-")[-1] + "_" + slug(concept.english)
             args.writer.add_concept(
-                    ID=idx,
-                    Name=concept.english,
-                    Concepticon_ID=concept.concepticon_id,
-                    Concepticon_Gloss=concept.concepticon_gloss
-                    )
+                ID=idx,
+                Name=concept.english,
+                Concepticon_ID=concept.concepticon_id,
+                Concepticon_Gloss=concept.concepticon_gloss,
+            )
             concepts[concept.id] = idx
         args.log.info("added concepts")
 
-        # (3) add language
-        args.writer.add_language(
-                ID="Caijia",
-                Glottocode="caij1234",
-                Name="Caijia"
-                )
-        args.log.info("added languages")
-
-        # (4) add forms
-        # provide corrections for the data
-        corrections = {
-                "tsh": "tsʰ",
-                "tɕh": "tɕʰ",
-                "kh": "kʰ",
-                "th": "tʰ",
-                "ph": "pʰ",
-                }
-
         # read in data
-        data = self.raw_dir.read_csv(
-                "data.tsv",
-                delimiter="\t", dicts=True)
-
-        # extend corrections
-        for i, row in enumerate(data):
-            form = row["Caijia (segments  separated)"]
-            for s, t in zip("12345", "¹²³⁴⁵"):
-                form = form.replace(s, t)
-            row["Segments"] = [corrections.get(c, c) for c in form.split()]
+        data = self.raw_dir.read_csv("data.tsv", delimiter="\t", dicts=True)
 
         # add data
         for entry in data:
-            if entry["Id"].strip():
-                cid = entry["Id"]
-            if entry["Caijia"].strip() != "/":
-                args.writer.add_form_with_segments(
-                        Language_ID="Caijia",
-                        Parameter_ID=concepts[cid],
-                        Value=entry["Caijia"],
-                        Form=entry["Caijia"],
-                        Segments=entry["Segments"],
-                        Source="Lee2023"
-                        )
+            args.writer.add_form_with_segments(
+                Language_ID=entry["DOCULECT"],
+                Parameter_ID=concepts["ID"],
+                Value=entry["IPA"],
+                Form=entry["IPA"],
+                Segments=entry["TOKENS"],
+                Source="Kim2011",
+            )
--- setup.py
+++ setup.py
@@ -5,22 +5,20 @@ with open("metadata.json", encoding="utf-8") as fp:
     metadata = json.load(fp)
 
 setup(
-    name='lexibank_kimkokborok',
-    py_modules=['lexibank_kimkokborok'],
+    name="lexibank_kimkokborok",
+    py_modules=["lexibank_kimkokborok"],
     include_package_data=True,
-    url=metadata.get("url",""),
+    url=metadata.get("url", ""),
     zip_safe=False,
     entry_points={
-        'lexibank.dataset': [
-            'kimkokborok=lexibank_kimkokborok:Dataset',
+        "lexibank.dataset": [
+            "kimkokborok=lexibank_kimkokborok:Dataset",
         ]
     },
-    install_requires=[
-        "pylexibank>=3.0.0"
-    ],
+    install_requires=["pylexibank>=3.0.0"],
     extras_require={
-        'test': [
-            'pytest-cldf',
+        "test": [
+            "pytest-cldf",
         ],
     },
 )
